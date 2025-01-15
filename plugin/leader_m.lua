532b8e00c6e953539f4cd477fedd7a7fe43cd161
peter-lyr <llydrp.ldp@gmail.com>
2025-01-15 16:55:08 +0800

    <leader>mp<leader>

--- plugin/leader_m.lua
+++ plugin/leader_m.lua
@@ -6,4 +6,9 @@ require 'which-key'.register {
   ['<leader>mn<leader>'] = { function() require 'mini.map'.toggle() end, 'minimap.toggle', mode = { 'n', 'v', }, },
   ['<leader>mnc'] = { function() require 'mini.map'.close() end, 'minimap.close', mode = { 'n', 'v', }, },
   ['<leader>mnf'] = { function() require 'mini.map'.toggle_focus() end, 'minimap.toggle_focus', mode = { 'n', 'v', }, },
+
+  ['<leader>mp'] = { name = 'markdown.preview', },
+  ['<leader>mpp'] = { '<cmd>MarkdownPreview<cr>', 'markdown.preview: preview', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>mps'] = { '<cmd>MarkdownPreviewStop<cr>', 'markdown.preview: stop', mode = { 'n', 'v', }, silent = true, },
+  ['<leader>mp<leader>'] = { '<cmd>MarkdownPreviewToggle<cr>', 'markdown.preview: toggle', mode = { 'n', 'v', }, silent = true, },
 }
